63bc498fb7420a092332bb8d737568068a4d70a0
Juan <juanpablo140204@outlook.com>
2026-05-15 17:21:06 -0300

    creating operations for entity Account

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,5 +1,61 @@
 package entities;
 
-public class Account {
+import java.util.List;
 
+public class Account {
+	private int accountNumber;
+	private String accountHolder;
+	private double value;
+	
+	private static int nextAccountNumber = 1; 
+	
+	public Account(String accountHolder) {
+		this.accountNumber = nextAccountNumber++;
+		this.accountHolder = accountHolder;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public String getAccountHolder() {
+		return accountHolder;
+	}
+
+	public void setAccountHolder(String accountHolder) {
+		this.accountHolder = accountHolder;
+	}
+	
+	public double getValue() {
+		return value;
+	}
+
+	public void deposit(double value) {
+		this.value += value;
+	}
+	
+	public void withdraw(double value) {
+		this.value -= value;
+	}
+	
+	public int transferTo(double value, List<Account> list, int numberCount) {
+		int result = -1;
+		
+		Account account = Account.findAccount(list, numberCount);
+		
+		if (account != null) {
+			withdraw(value);
+
+			account.deposit(value);
+			
+			result = numberCount;
+		}
+		
+		return result;
+	}
+	
+	public static Account findAccount(List<Account> list, int numberCount) {
+		return list.stream().filter(x -> x.getAccountNumber() == numberCount).findFirst().orElse(null);
+	}
+	
 }
